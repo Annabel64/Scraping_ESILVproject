084871177e7d467f1b350479ab1d9cbf346ca900
Annabel64 <76532104+Annabel64@users.noreply.github.com>
2023-01-03 17:21:41 +0100

    ajout nom et image

--- test_bs4.py
+++ test_bs4.py
@@ -1,8 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.leboncoin.fr/recherche?text=iphone%2012"
+url = "https://ledenicheur.fr/search?search=iphone%2012"
 html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
-prices = soup.find_all(class_='_137P- _3j0OU P4PEa')
-print(prices) #si affiche liste vide = pas bon signe
+prices = soup.find_all(class_="Text--1d9bgzp ilIDIB")
+nom=soup.find_all(class_="Text--1d9bgzp bYZpdo titlesmalltext")
+img = soup.find(class_="ImageContainer-sc-4o01vu-2 WqEZk")
+price = None
+monnaie = None
+if prices:
+    monnaie = prices[0].text.split('\xa0')[1]
+    prices = [prices[i].text.replace(",", ".") for i in range(len(prices))]
+    prices = [float(price.split('\xa0')[0]) for price in prices]
+    price = prices[0]
+
+
+
+print("prix : ", price)
+print("monnaie : ", monnaie)
+print("image : ", img)
+print("nom : ", nom[0].text)
