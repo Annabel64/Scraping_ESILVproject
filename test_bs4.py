d0947fbdf0b1c3c1177e14200dd4dcd50de77e00
Rémi <remi.lombard@edu.devinci.fr>
2023-01-10 15:24:38 +0100

    Merge

--- test_bs4.py
+++ test_bs4.py
@@ -1,23 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://ledenicheur.fr/search?search=iphone%2012"
+url = "https://ledenicheur.fr/product.php?p=5683804"
 html = requests.get(url).text
+
+
 soup = BeautifulSoup(html, 'html.parser')
-prices = soup.find_all(class_="Text--1d9bgzp ilIDIB")
-nom=soup.find_all(class_="Text--1d9bgzp bYZpdo titlesmalltext")
-img = soup.find(class_="ImageContainer-sc-4o01vu-2 WqEZk")
-price = None
-monnaie = None
-if prices:
-    monnaie = prices[0].text.split('\xa0')[1]
-    prices = [prices[i].text.replace(",", ".") for i in range(len(prices))]
-    prices = [float(price.split('\xa0')[0]) for price in prices]
-    price = prices[0]
+# prices = soup.find_all(class_="Text--1d9bgzp ilIDIB")
+# nom=soup.find_all(class_="Text--1d9bgzp bYZpdo titlesmalltext")
+# Find class that contains 'FirstImage'
+img = soup.find_all('img')
+alt = [i for i in img if i.get('alt') == "Apple iPhone 13 5G 4Go RAM 128Go"]
+print(alt[0].get('src'))
+# price = None
+# monnaie = None
+# if prices:
+#     monnaie = prices[0].text.split('\xa0')[1]
+#     prices = [prices[i].text.replace(",", ".") for i in range(len(prices))]
+#     prices = [float(price.split('\xa0')[0]) for price in prices]
+#     price = prices[0]
 
-
-
-print("prix : ", price)
-print("monnaie : ", monnaie)
-print("image : ", img)
-print("nom : ", nom[0].text)
+# print("prix : ", price)
+# print("monnaie : ", monnaie)
+# print("image : ", img)
+# print("nom : ", nom[0].text)
